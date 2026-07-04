c4f3457ae21532f9e8bae515789b8302ddc8d4a0
jamo <jgormo@outlook.com>
2026-07-04 20:18:57 +0930

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        close_to_open = {'}':'{', ']':'[', ')':'('}
+
+        stack = []
+
+        for ch in s:
+            if ch in close_to_open:
+                if stack and stack[-1] == close_to_open[ch]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ch)
+
+        return True if not stack else False
